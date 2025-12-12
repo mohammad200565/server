df96ff5466e7642fb6277381933d65b322586dbb
YAZAN420 <yazanmahfooz8@gmail.com>
2025-12-12 23:57:18 +0300

    ..

--- app/Http/Controllers/api/DepartmentController.php
+++ app/Http/Controllers/api/DepartmentController.php
@@ -24,6 +24,7 @@ class DepartmentController extends BaseApiController
     }
     public function store(StoreDepartmentRequest $request)
     {
+        $this->authorize('create');
         $validated = $request->validated();
 
         $validated['user_id'] = $request->user()->id;
